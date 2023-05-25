74399e81e18b8cdd4ccfe29ac70eeb1fa1cd546d
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-24 21:19:21 -0700

    feat: add orca image

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import './style/style.css';
+import Orca from './orca.png';
 
 const main = () => {
     const element = document.createElement('h1');
     element.textContent = 'Hello, webpack!';
     element.classList.add('hello');
     document.body.appendChild(element);
+
+    const orcaImage = new Image();
+    orcaImage.src = Orca;
+    document.body.appendChild(orcaImage);
 }
 window.addEventListener('load', main);
