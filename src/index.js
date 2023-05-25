df3f374e761d84b449e0c2c35aeced340c3e72e1
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-24 18:47:52 -0700

    feat: import 'style.css' into 'index.js'

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import './style/style.css';
+
 const main = () => {
     const element = document.createElement('h1');
     element.textContent = 'Hello, webpack!';
+    element.classList.add('hello');
     document.body.appendChild(element);
 }
 window.addEventListener('load', main);
