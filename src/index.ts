1efe0c64786fa9b847aa67200b5075f614c3da3a
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-27 15:57:24 -0700

    build: change index.js to index.ts

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import './style/style.css';
+import printMe from './print';
+
+const main = () => {
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Hello, webpack!';
+  h1.classList.add('hello');
+  document.body.appendChild(h1);
+
+  const btn = document.createElement('button');
+  btn.textContent = 'Click me!';
+  btn.addEventListener('click', printMe);
+  document.body.appendChild(btn);
+};
+window.addEventListener('load', main);
