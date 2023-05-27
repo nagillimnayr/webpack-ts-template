92484f96fe99aaf20d5b4f7cca3d9e016283489f
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-27 14:01:11 -0700

    build: install prettier

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import './style/style.css';
-import Orca from './orca.png';
+import printMe from './print';
+import { doc } from 'prettier';
 
 const main = () => {
-    const element = document.createElement('h1');
-    element.textContent = 'Hello, webpack!';
-    element.classList.add('hello');
-    document.body.appendChild(element);
+  const h1 = document.createElement('h1');
+  h1.textContent = 'Hello, webpack!';
+  h1.classList.add('hello');
+  document.body.appendChild(h1);
 
-    const orcaImage = new Image();
-    orcaImage.src = Orca;
-    document.body.appendChild(orcaImage);
-}
+  const btn = document.createElement('button');
+  btn.textContent = 'Click me!';
+  btn.addEventListener('click', printMe);
+  document.body.appendChild(btn);
+};
 window.addEventListener('load', main);
