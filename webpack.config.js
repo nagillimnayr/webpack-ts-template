0983fd9a96634d40a195a46e8902336b5750b90e
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-29 09:51:27 -0700

    refactor: rename 'index.'ts' to 'main.ts'

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.ts',
+    app: './src/main.ts',
   },
   devtool: 'inline-source-map',
   devServer: {
