ceae701f8f594b8fdf0d0ce09e1284ffba17b147
Ryan Milligan <ryanmilligan88@gmail.com>
2023-05-29 10:22:43 -0700

    build: add source-map-loader' to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -31,10 +31,14 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.js$/i,
+        use: 'source-map-loader',
+      },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    extensions: ['.webpack.js', '.web.js', '.tsx', '.ts', '.js', '.jsx'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
